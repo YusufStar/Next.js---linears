7a87844292732b00bec2ff90e08e4adb0e632594
Yusuf Yıldız <ysfyldz0077@gmail.com>
2023-06-15 22:26:43 +0300

    create basic next app

---
+++ components/hero.tsx
@@ -0,0 +1,11 @@
+interface  HeroProps {
+    title: string;
+    subtitle: string;
+}
+
+export const Hero = ({ title, subtitle } : HeroProps) => {
+    return <div>
+        <h1>{title}</h1>
+        <p>{subtitle}</p>
+    </div>
+}
